3fe6d89e968bec4c7cc48a6d62a229193e727d68
nbergstrand <bergstrand.niklas@gmail.com>
2021-05-03 09:01:07 +0100

    Added actors and cameras

--- Assets/The Great Fleece/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Assets/The Great Fleece/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b105413efb26a054d969190c1e580534ca497bdff4755b991263de16e56f9497
-size 1091
+oid sha256:005fa7056c5830ef264a916dc2cd177e8a518b01d0f159cea911ed88f1fe57e0
+size 1122

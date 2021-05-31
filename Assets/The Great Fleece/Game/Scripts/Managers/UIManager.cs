d69673981c87b13b5f19632bdacfbe19ef38f0d8
nbergstrand <bergstrand.niklas@gmail.com>
2021-05-31 16:08:32 +0100

    Added loading screen

--- Assets/The Great Fleece/Game/Scripts/Managers/UIManager.cs
+++ Assets/The Great Fleece/Game/Scripts/Managers/UIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6b99222086ca7f3a33772bef482bd68ee3b48507e0dddbe2868fccd929ac18d
-size 704
+oid sha256:2c56b1a1706b9857bc36ec9462b1d31fe2bd9f0b4eaf9642d2fb4927460d824a
+size 806
---
+++ Assets/The Great Fleece/Game/Scripts/UI/LoadLevel.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3aae2fde24aa7d63a81e93375d8dc636a6bfbef4ebdec85ab9f850c9ed61e032
+size 625
